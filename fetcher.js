32068ffb11dbdc1a12fe0e1edba8092f0c7d0e1f
Ren Guidolin <8.renji@gmail.com>
2021-12-02 13:33:31 -0500

    created log statement

--- fetcher.js
+++ fetcher.js
@@ -2,18 +2,18 @@ let arg = process.argv.slice(2, 4);
 const request = require('request');
 const fs = require('fs');
 
-request(`${arg[0]}`, (error, response, body) => {
+request(arg[0], (error, response, body) => {
   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  fs.writeFile(`${arg[1]}`, body, err => {
+  fs.writeFile(arg[1], body, err => {
     if (err) {
       console.error(error);
       return;
     }
     //file written successfully
+    console.log(`File downloaded and saved ${body.length} bytes to ${arg[1]}`);
   });
 });
 
 
 
 // https://www.youtube.com/
-// /Users/renji/Lighthouse/Week2/Day3/page-fetcher
